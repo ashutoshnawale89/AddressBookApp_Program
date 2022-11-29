250f96ee4f87829bfc8a85452ba8c2b6aaa01707
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-29 11:12:41 +0530

    Refactor:On Click Submit Button Save To The Local Storage

--- AddressBook.js
+++ AddressBook.js
@@ -1,83 +1,71 @@
-function hello(event)
-{
-    event.preventDefault();
+
+function validation(){
+    let demo=false;
+    let name=document.getElementById('name1').value;
+    var address=document.getElementById('address1').value;
+    var pincode=document.getElementById('pincode1').value;
+    var number=document.getElementById('number1').value;
     let firstNameRegex = /^[A-Z]{1}[a-z]{2,}$/;
-    let name = document.getElementById('name').value;
-
-    if(firstNameRegex.test(name))
-     {
-        localStorage.setItem("name",name);
-        alert("Hello, "+ localStorage.getItem("name"));
-     }else
-     {
-        alert("Enter valid name");
-        document.getElementById("name-err").innerHTML ="Min 3 chars & first letter capital!"
-    }
-       // For Address Regex 
-     event.preventDefault();
-     let addressRegex = /^[A-Z]{1}[a-z]{5,6}(- _ .)[A-Z]$^/;
-    let address = document.getElementById('address').value;
-    if(addressRegex.test(address))
-    {
-        localStorage.setItem("address",name);
-        alert("Hello, "+localStorage.getItem("address"));
-    }else{
-        alert("Enter Valid Address");
-        document.getElementById('address-err').innerHTML ="Min  6 chars & First Letter capital"
-    }
-    // For Number regex 
-    event.preventDefault();
-    let Number = /(91)()[6-9]{1}[0-9]{9}$^/;
-   let number = document.getElementById('number').value;
-   if(Number.test(number))
-   {
-       localStorage.setItem("number",number);
-       alert("Hello, "+localStorage.getItem("number"));
-   }else{
-       alert("Enter Valid number");
-       document.getElementById('number-err').innerHTML =" country code  & first Number 6 - 9 "
+    let addressRegex = /^[A-Z]{1}[a-zA-Z0-9./]{5,}$/;
+    let Number = /[9]?[1]?[6-9]{1}[0-9]{9}$/;
+    let PinCode=/^[0-9]{6}$/;
+ 
+    let nameError=document.getElementById('name-err');
+ 
+ if(firstNameRegex.test(name)){
+    if(addressRegex.test(address)){
+     if(Number.test(number)){
+       if(PinCode.test(pincode)){
+        demo=true;
+        if(demo==true){
+            var name2=$('#name1').value;
+                var address2=$('#address1').value;
+                var state2=$('#state1').value;
+                var pincode2=$('#pincode1').value;
+                var city2=$('#city1').value;
+                var phoneNumber2=$('#number1').value;
+            
+                var jsonObject={
+                    Name:"",
+                    Address:"",
+                    State:"",
+                    Pincode:"",
+                    City:"",
+                    PhoneNumber:""
+                };
+                jsonObject.Name=name2;
+                jsonObject.Address=address2;
+                jsonObject.State=state2;
+                jsonObject.Pincode=pincode2;
+                jsonObject.City=city2;
+                jsonObject.PhoneNumber=phoneNumber2;
+            
+                var str=JSON.stringify(jsonObject);
+                localStorage.setItem("AddressBook Details",str);
+                alert("Data Save succefully");
+        }
+        else {
+            alert("Data Not Save In Local Storage ");
+        }
+        
+       }
+       else{
+           alert('Invalid PinCode');
+            return false;
+           }
+         }
+     else{
+       alert('Invalid Number');
+          return false;
+     }
    }
+   else{
+     alert('Invalid Address');
+        return false;
+ }
+ }
+ else{
+   alert('Invalid UserName');
+   return false;
+}     
 }
-
-function check(event)
-{
-    event.preventDefault();
-    let firstNameRegex = /^[A-Z]{1}[a-z]{2,}$/;
-    let name = document.getElementById('name').value;
-
-    if(!firstNameRegex.test(name))
-    document.getElementById("name-err").innerHTML ="Min 3 chars & first letter capital!"
-   else
-   document.getElementById("name-err").innerHTML =""
-}
-document.getElementById('form')
-   .addEventListener('submit',hello);
-// for Address Regex 
-   function check(event)
-{
-    event.preventDefault();
-    let addressRegex = /^[A-Z]{1}[a-z]{5,6}(- _ .)[A-Z]$^/;
-   let address = document.getElementById('address').value;
-
-    if(!addressRegex.test(address))
-    document.getElementById("address-err").innerHTML ="Min 6 chars & first letter capital!"
-   else
-   document.getElementById("addresss-err").innerHTML =""
-}
-document.getElementById('form')
-   .addEventListener('submit',hello);
-   // Number regex 
-   function check(event)
-   {
-    event.preventDefault();
-    let Number = /(91)()[6-9]{1}[0-9]{9}$^/;
-   let number = document.getElementById('number').value;
-       if(!Number.test(number))
-       document.getElementById("number-err").innerHTML ="Min 6 chars & first letter capital!"
-      else
-      document.getElementById("number-err").innerHTML =""
-   }
-   document.getElementById('form')
-      .addEventListener('submit',hello);
-    function addEventListener();
-    document.getElementById("button").innerHTML ="Submitted Successfully"
